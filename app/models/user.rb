c5c9129b372944c1356724d96c169ba618c9e880
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-09-07 16:16:41 -0400

    added has secure password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :wines
   has_many :reviews
   has_many :reviewed_wines, through: :reviews, source: :wine 
-
+  has_secure_password
  # has_many :categories, through: :wines
 
 end
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,6 +1,6 @@
 class Wine < ApplicationRecord
   belongs_to :user
-  belongs_to :category
+  # belongs_to :category
   has_many :reviews
   has_many :users, through: :reviews 
 end
