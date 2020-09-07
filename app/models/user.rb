78650f08343c9ec1695d68d27641596bd3b395fb
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-09-07 15:54:41 -0400

    set up model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :wines
+  has_many :reviews
+  has_many :reviewed_wines, through: :reviews, source: :wine 
+
+ # has_many :categories, through: :wines
+
 end
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,4 +1,6 @@
 class Wine < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :reviews
+  has_many :users, through: :reviews 
 end
