220e0e3b12ed9e29884cf9edc4dbeb209aec9e51
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-09-08 13:52:28 -0400

    added create action to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
 
+  def create
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id 
+      redirect_to user_path(user)
+    else
+        flash[:message] = "Invalid entry, please try again"
+      redirect_to "/login"
+    end 
+  end
+
   def destroy
     session.clear
     redirect_to root_path
