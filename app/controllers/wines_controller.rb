c1f38377a42b9d134d8ececefda43c3018dde296
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-09-08 14:46:09 -0400

    added create action and wine params to wines controller

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -6,4 +6,19 @@ class WinesController < ApplicationController
     @wine = Wine.new
   end
 
+  def create
+    @wine = Wine.new(wine_params)
+    if @wine.save
+      redirect_to @wine 
+    else 
+      render :new 
+    end
+  end
+
+  private
+
+  def wine_params
+    params.require(:wine).permit(:name, :img, :content)
+  end
+
 end
