f4ea9c223326dff3b83cf439c949edd86d3f527e
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-09-10 16:21:16 -0400

    added reviews show view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -50,7 +50,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:rating, :notes, :wine_id, :user_id)
+    params.require(:review).permit(:rating, :notes, :wine_id)
   end
 
 
