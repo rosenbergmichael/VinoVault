35757342ea1da5d619ca33347ff59f104293325b
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-09-12 11:10:31 -0400

    changed review destroy action to redirect to review path

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -48,7 +48,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
-    redirect_to wine_path(@review.wine)
+    redirect_to review_path 
   end
  
   private
